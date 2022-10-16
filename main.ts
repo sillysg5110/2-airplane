5093a840c4009031af37a8292a8912d3d5687649
刘权 <bbs.codertown.cn@gmail.com>
2022-10-16 16:23:07 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,28 @@
+input.onButtonPressed(Button.A, function () {
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 119)
+})
+radio.onReceivedString(function (receivedString) {
+    item = "receivedString"
+    if ("C" == item) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+    } else if ("D" == item) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+    } else if ("0" == item) {
+        SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
+})
+let item = ""
+radio.setGroup(77)
+radio.setTransmitPower(7)
+basic.showLeds(`
+    # . . . #
+    . # . # #
+    . . # . #
+    . # . # #
+    # . . . #
+    `)
